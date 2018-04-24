d1d46c48079cea4eb928feb31789fb42121e8761
Phanvan <phanvan91@gmail.com>
2018-04-24 18:27:47 +0700

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // $this->call(UsersTableSeeder::class);
+
 
         Model::unguard();
         DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        $this->call(UsersTableSeeder::class);
          $this->call(CategoriesTableSeeder::class);
          $this->call(ManufacturersTableSeeder::class);
          $this->call(ProductsTableSeeder::class);
