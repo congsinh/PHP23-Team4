ba912f410a1d457c0bf23c82a8c178e27b51e195
phanvan91 <phanvan91@gmail.com>
2018-04-24 17:43:49 +0700

    update

---
+++ resources/views/pages/include/menu.blade.php
@@ -0,0 +1,26 @@
+<div class="mainmenu-area">
+    <div class="container">
+        <div class="row">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="../home.blade.php">Home</a></li>
+                    <li><a href="../shop.blade.php">Shop page</a></li>
+                    <li><a href="../single_product.blade.php">Single product</a></li>
+                    <li><a href="../cart.blade.php">Cart</a></li>
+                    <li><a href="../cart.blade.php">Checkout</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Others</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div> <!-- End mainmenu area -->
