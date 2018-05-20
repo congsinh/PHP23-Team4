134307ce368fb31706c70fa7048fa431e696cb93
phanvan91 <phanvan91@gmail.com>
2018-05-20 17:02:56 +0700

    fix_home_page

--- resources/views/pages/single_product.blade.php
+++ resources/views/pages/single_product.blade.php
@@ -52,8 +52,8 @@
                                             </div>
                                         </div>
 
-                                        <form action="" class="cart">
-                                            <button class="add_to_cart_button" type="submit">Thêm giỏ hàng</button>
+                                        <form class="cart">
+                                            <button class="add_to_cart_button tryMe"  value="{{ $product->id }}"  type="button">Thêm giỏ hàng</button>
                                         </form>
                                         <div class="config">
                                             <h3 class="">Thông số kỹ thuật</h3>
